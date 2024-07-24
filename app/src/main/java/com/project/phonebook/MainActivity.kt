3bab85317ff3fadb8b104d73fec22e2b0bfd64b5
ImGaram <gim78014@gmail.com>
2024-07-24 20:22:17 +0900

    추가: 알람 권한 요청 기능

--- app/src/main/java/com/project/phonebook/MainActivity.kt
+++ app/src/main/java/com/project/phonebook/MainActivity.kt
@@ -1,10 +1,18 @@
 package com.project.phonebook
 
+import android.content.Intent
+import android.os.Build
+import android.os.Build.VERSION_CODES
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationManagerCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.phonebook_app.Fragment.ContactDetailFragment
+import com.project.phonebook.data.ContractData
 import com.project.phonebook.databinding.ActivityMainBinding
 import com.project.phonebook.fragment.MainFragment
 
@@ -13,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    @RequiresApi(VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -23,6 +32,22 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
-        supportFragmentManager.beginTransaction().replace(R.id.main_fcv, MainFragment(), "MAIN").commitNow()
+        initNotificationPermission()
+
+        val notificationExtraData = intent.getParcelableExtra("notificationClick", ContractData::class.java)
+        if (notificationExtraData != null)
+            supportFragmentManager.beginTransaction().replace(R.id.main_fcv, ContactDetailFragment()).commitNow()
+        else supportFragmentManager.beginTransaction().replace(R.id.main_fcv, MainFragment(), "MAIN").commitNow()
+    }
+
+    private fun initNotificationPermission() {
+        if (Build.VERSION.SDK_INT >= VERSION_CODES.TIRAMISU) {
+            if (!NotificationManagerCompat.from(this).areNotificationsEnabled()) {
+                val intent = Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS).apply {
+                    putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+                }
+                startActivity(intent)
+            }
+        }
     }
 }
