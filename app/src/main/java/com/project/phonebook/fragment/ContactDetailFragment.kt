f1b2b098cb38a8ef342a893d8d62f13fc381aafb
ImGaram <gim78014@gmail.com>
2024-07-25 19:08:58 +0900

    수정: 데이터를 받는 코드 추가

--- app/src/main/java/com/project/phonebook/fragment/ContactDetailFragment.kt
+++ app/src/main/java/com/project/phonebook/fragment/ContactDetailFragment.kt
@@ -1,12 +1,16 @@
 package com.project.phonebook.fragment
 
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 
 import com.project.phonebook.R
+import com.project.phonebook.data.ContractData
 import com.project.phonebook.databinding.FragmentContactDetailBinding
 
 
@@ -21,10 +25,14 @@ class ContactDetailFragment : Fragment() {
 //        title.add(DetailContentTitleData("Phone Number", "Event", "E-Mail"))
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val arg = arguments?.getParcelable("contact", ContractData::class.java)
+        Log.d("TAG", "onCreateView: $arg")
+
         return inflater.inflate(R.layout.fragment_contact_detail,container,false)
 
 
